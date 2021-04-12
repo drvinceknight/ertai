e3a8f599e8c45a824345b53578cf1fd42216a6db
Vince Knight <vincent.knight@gmail.com>
2021-04-12 13:30:03 +0100

    Write tests for tapping cards.

--- tests/test_card.py
+++ tests/test_card.py
@@ -11,12 +11,26 @@ def test_base_card_class_has_expected_attributes_and_defaults():
     card = ertai.Card()
     assert card.title is None
     assert card.cost == 0
+    assert card.tapped is False
 
 
-def test_base_card_class_initialises():
+def test_base_card_class_takes_expected_attribute_on_init():
     """
     This is a test to check that the base card class initialises as expected
     """
     card = ertai.Card(title="Island")
     assert card.title == "Island"
     assert card.cost == 0
+    assert card.tapped is False
+
+
+def test_base_card_class_taps():
+    """
+    This is a test to check the `tap` method works.
+    """
+    card = ertai.Card()
+    assert card.tapped is False
+    card.tap()
+    assert card.tapped is True
+    card.untap()
+    assert card.tapped is False
