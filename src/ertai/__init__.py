b5cdbf13876dbf20c292b30140ad8d91434b46f6
Vince Knight <vincent.knight@gmail.com>
2021-04-12 15:43:46 +0100

    Add colors.

--- src/ertai/__init__.py
+++ src/ertai/__init__.py
@@ -1,4 +1,14 @@
 """A tool for mathematical analysis of Magic the Gathering"""
-from .card import Card
+from .card import BasicLand, Card
 
-__version__ = "0.0.1"
+from typing import Tuple
+
+__version__ = "0.0.1"  # type: str
+
+colors = (
+    "Blue",
+    "Red",
+    "Black",
+    "White",
+    "Green",
+)  # type: Tuple[str, str, str, str, str]
