4fb4201cded2f19c1b9778cca7b3859b7055d6ea
Vince Knight <vincent.knight@gmail.com>
2021-04-12 13:31:56 +0100

    Add functionality to tap a card.

--- src/ertai/card.py
+++ src/ertai/card.py
@@ -13,3 +13,12 @@ class Card:
 
     title: Union[str, None] = None
     cost: int = 0
+    tapped: bool = False
+
+    def tap(self):
+        """A method to tap a card"""
+        self.tapped = True
+
+    def untap(self):
+        """A method to untap a card"""
+        self.tapped = False
