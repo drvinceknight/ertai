77398ac34034778d68b34fd7e4b9a6173db72409
Vince Knight <vincent.knight@gmail.com>
2021-04-13 11:32:36 +0100

    Separate land and card tests.

--- tests/test_card.py
+++ tests/test_card.py
@@ -54,34 +54,3 @@ def test_base_card_class_can_be_cast_when_not_given_sufficient_mana():
     pool = ertai.Mana("Blue", "Red", "Red")
     counter_spell = ertai.Card(title="Counter Spell", cost=ertai.Mana("Blue", "Blue"))
     assert counter_spell.cast(pool=pool) == pool
-
-
-def test_land_initialisation():
-    """
-    For all colors test that lands have given color and enter untapped.
-    """
-    for color in ertai.colors:
-        card = ertai.BasicLand(color=color)
-        assert card.color == color
-        assert card.tapped is False
-
-
-def test_land_can_add_mana_of_given_color_if_untapped():
-    """
-    For all colors test that lands can give required color.
-    """
-    for color in ertai.colors:
-        card = ertai.BasicLand(color=color)
-        assert card.generate_mana() == ertai.Mana(color)
-        assert card.tapped is True
-
-
-def test_land_cannot_add_mana_of_given_color_if_tapped():
-    """
-    For all colors test that lands cannot give mana if tapped.
-    """
-    for color in ertai.colors:
-        card = ertai.BasicLand(color=color)
-        card.tap()
-        assert card.generate_mana() is None
-        assert card.tapped is True
