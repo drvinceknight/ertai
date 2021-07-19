76eb72fde6686b27d43783e223cb18564f222f0b
henry <z1762301168@163.com>
2021-07-19 15:30:23 +0100

    Create a new class represent creature card in MTGA

---
+++ src/ertai/creature.py
@@ -0,0 +1,98 @@
+"""
+Class for Creature
+"""
+
+from .card import Card
+from .mana import Mana
+
+
+class Creature(Card):
+    """
+    A class for basic creatures.
+    """
+
+    def __init__(
+        self,
+        title=None,
+        cost=Mana(),
+        tapped=False,
+        power=0,
+        toughness=0,
+    ):
+        """
+        Initiate this creature by giving power, toughness of it.
+
+        Other basic information below of this Creature derived from Card:
+
+        title : title of this creature
+
+        cost : cost to cast this creature
+
+        tapped : tap information of this creature
+
+        Parameters:
+            - power : power  of this creature
+            - toughness : Toughness value of this creature,
+                but this value can be modified in a fight or other condition
+            - base_toughness : Toughness value of this creature,
+                use to store the base toughness of this creature
+        """
+
+        Card.__init__(self, title, cost, tapped)
+
+        self.power = power
+        self.toughness = toughness
+        self.base_toughness = toughness
+        self.state = True
+
+    def __repr__(self):
+        """
+        This is used to print the information about this creature.
+        """
+        return (
+            self.title
+            + "\t"
+            + "'t Cost:"
+            + str(self.cost)
+            + "\t"
+            + "\t Power:"
+            + str(self.power)
+            + "\t Toughness:"
+            + str(self.toughness)
+        )
+
+    def check_state(self):
+        """
+        This function is used after a attack happended.
+        If the toughness of this creature drop down to 0 or less,
+        then we set it's state to False which means that this creature is died.
+        """
+        if self.toughness <= 0:
+            self.state = False
+        else:
+            self.toughness = self.base_toughness
+
+    def attack(self, target) -> int:
+        """
+        A simulation of this creature fight with other one.
+        This function also be called on the target creature when fight happen.
+        It returns a int value represent the damage opponent player taken.
+        Also,if no Creature block this attack from this creature,
+        then the opponent would take the damage
+        as this creature's power value
+
+        Parameters:
+            - target : Another creature this one fight with
+        """
+
+        if self.tapped is True:
+            return 0
+
+        if not target:
+            return self.power
+        else:
+            self.toughness = self.toughness - target.power
+            self.check_state()
+        self.tap()
+
+        return 0
---
+++ tests/test_creature.py
@@ -0,0 +1,79 @@
+"""
+Tests for Creatures.
+"""
+
+
+import ertai
+
+
+def test_creature_has_expected_attributes_and_defaults():
+    """
+    This is a test to check the no-arguments
+    initialised creature has all the attributes we expected.
+    """
+    creature = ertai.Creature()
+
+    assert creature.title is None
+    assert creature.cost == ertai.Mana()
+    assert creature.tapped is False
+    assert creature.power == 0
+    assert creature.toughness == 0
+    assert creature.base_toughness == 0
+
+
+def test_creature_takes_expected_attribute_on_init():
+    """
+    This is a test to check wether the
+    full-attributes initialisation is working.
+    """
+    creature = ertai.Creature(
+        title="Selfless Savior",
+        cost=ertai.Mana("White"),
+        tapped=False,
+        power=1,
+        toughness=1,
+    )
+
+    assert creature.title == "Selfless Savior"
+    assert creature.cost == ertai.Mana("White")
+    assert creature.tapped is False
+    assert creature.power == 1
+    assert creature.toughness == 1
+    assert creature.base_toughness == 1
+
+
+def test_creature_attack():
+    """
+    Tests the situation when a fight happend bewteen two creatures.
+    """
+    my_creature_1 = ertai.Creature(
+        title="Selfless Savior",
+        cost=ertai.Mana("White"),
+        tapped=False,
+        power=1,
+        toughness=1,
+    )
+
+    my_creature_2 = ertai.Creature(
+        title="Usher of the Fallen",
+        cost=ertai.Mana("White"),
+        tapped=False,
+        power=2,
+        toughness=1,
+    )
+
+    target_creature = ertai.Creature(
+        title="Wall of Runes",
+        cost=ertai.Mana("Blue"),
+        tapped=False,
+        power=0,
+        toughness=4,
+    )
+
+    damage_to_player_1 = my_creature_1.attack(my_creature_2)
+    damage_to_player_2 = my_creature_2.attack(target_creature)
+
+    assert damage_to_player_1 == 0
+    assert damage_to_player_2 == 0
+    assert my_creature_1.state is False
+    assert my_creature_2.state is True
